972ba664d621634acf1fc81382da86eed93279ce
dede2008w <tomypeekblinders@gmail.com>
2024-10-30 14:22:52 -0300

    variaveis

--- TowerDefence/Assets/EnemySpawner.cs
+++ TowerDefence/Assets/EnemySpawner.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond =
 }
