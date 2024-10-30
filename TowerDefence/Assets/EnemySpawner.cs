4aeddf761dfeeab1041706542edefaf07d55d078
dede2008w <tomypeekblinders@gmail.com>
2024-10-30 14:40:40 -0300

    Update EnemySpawner.cs

--- TowerDefence/Assets/EnemySpawner.cs
+++ TowerDefence/Assets/EnemySpawner.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+
     [Header("Attributes")]
     [SerializeField] private int baseEnemies = 8;
-    [SerializeField] private float enemiesPerSecond =
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+
+    private void Start()
+    {
+        enemiesLeftToSpawn = baseEnemies;
+    }
+
+    private int EnemiesPerWave()
+    {
+     return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, 0.75f ));
+     
+    }
+
 }
