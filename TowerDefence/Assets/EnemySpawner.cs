e0b0bf06e4f3d2ae56da796066921d686092b6bb
dede2008w <tomypeekblinders@gmail.com>
2024-10-31 13:47:09 -0300

    enemyspawner
    spawn criado

--- TowerDefence/Assets/EnemySpawner.cs
+++ TowerDefence/Assets/EnemySpawner.cs
@@ -11,20 +11,38 @@ public class EnemySpawner : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private int baseEnemies = 8;
     [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWavbew = 5f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
 
     private int currentWave = 1;
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
 
-    private void Start()
+    
+
+    private void Update()
     {
+        if (!isSpawning) return;
+
+        timeSinceLastSpawn += Time.deltaTime;  
+
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond))
+        {
+            Debug.Log("Spawn Enemy");
+        }
+    }
+
+    private void StartWave()
+    {
+        isSpawning = true;
         enemiesLeftToSpawn = baseEnemies;
     }
 
     private int EnemiesPerWave()
     {
-     return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, 0.75f ));
+     return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor ));
      
     }
 
