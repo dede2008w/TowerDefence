959655c050569735bfcf6d9e229771de67e29b6a
dede2008w <tomypeekblinders@gmail.com>
2024-11-01 15:33:48 -0300

    turret script criado

--- TowerDefence/Assets/Turret.cs
+++ TowerDefence/Assets/Turret.cs
@@ -1,18 +1,107 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+public class Turret : MonoBehaviour //Iatacavel
+
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] protected float targetingRange = 5f;
+
+    [SerializeField] protected LayerMask enemyMask;
+
+    [SerializeField] protected GameObject bulletPrefab;
+
+    [SerializeField] protected Transform firingPoint;
+
+    [SerializeField] private float bps = 1f;
+
+    protected Transform target;
+
+    protected float timeUntilFire;
+
+
+    public virtual void Atacar()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void Update()
+
     {
-        
+
+        if (target == null)
+
+        {
+
+            FindTarget();
+
+            return;
+
+        }
+
+        if (!CheckTargetIsInRange())
+
+        {
+
+            target = null;
+
+        }
+
+        else
+
+        {
+
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bps)
+
+            {
+
+                Shoot();
+
+                timeUntilFire = 0f;
+
+            }
+
+        }
+
     }
+
+    protected virtual void Shoot()
+
+    {
+
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+
+        //Bullet bulletScript = bulletObj.GetComponent<//Bullet>();
+
+        //bulletScript.SetTarget(target);
+
+    }
+
+    private bool CheckTargetIsInRange()
+
+    {
+
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+
+    }
+
+    private void FindTarget()
+
+    {
+
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+
+        {
+
+            target = hits[0].transform;
+
+        }
+
+    }
+
 }
